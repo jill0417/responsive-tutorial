8eadc4fb08e0d988c4d7f1b194d0d5339b089920
pin-0417 <s4023377@student.rmit.edu.au>
2025-10-01 11:05:09 +1000

    css update

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
 const modeButton = document.getElementById("modeButton");
+// this will keep track od dark vs light mode
+let isLightMode = true;
 
 // function to toggle between dark mode and light mode
 // takes in bool : if true set to dark if false set to light
-function switchModes(toDark){
-  if(toDark){
+function switchModes(toDark) {
+  if (toDark) {
     modeButton.textContent = "Light Mode";
+    isLightMode = false;
   } else {
     modeButton.textContent = "Dark Mode";
+    isLightMode = true;
   }
+}
+
+modeButton.addEventListener("click", () => {
+  switchModes(isLightMode);
+});
+
+// test to see if user prefers dark mode
+// if so update the lightmode variable, and our button text
+let userModePreference = window.matchMedia("(prefers-color-scheme: dark)");
+console.log(userModePreference.matches);
+if (userModePreferenc.matches) {
+  isLightMode = false;
+  switchModes(isLightMode);
 }
